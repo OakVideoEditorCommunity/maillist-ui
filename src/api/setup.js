3bc4c22d8087739c849283f8b64ddd391d686bc2
Mike Solar <iam@mikesolar.cn>
2026-05-28 19:47:00 +0800

    fix: add missing testDb method to setupApi

--- src/api/setup.js
+++ src/api/setup.js
@@ -2,5 +2,6 @@ import request from './request'
 
 export const setupApi = {
   status: () => request.get('/setup/status'),
+  testDb: (data) => request.post('/setup/test-db', data),
   setup: (data) => request.post('/setup/setup', data),
 }
